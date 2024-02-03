66cda795f12382e0236521ab88dcfd4863cb61ea
Nikolai Syrovezhko <syrovezhko@list.ru>
2024-02-03 17:04:25 +0300

    feat: general: `goodbye`
    After program work finished (ctrl + c pressed or user sent .exit command into console) the program displays the following text in the console: "Thank you for using File Manager, Username, goodbye!"

--- src/hello.js
+++ src/hello.js
@@ -1,17 +1,38 @@
+function handleExit(username) {
+  process.stdout.write(
+    `\nThank you for using the file manager, ${username}, goodbye!\n`,
+  );
+  process.exit(0);
+}
+
 function hello() {
+  let username = "";
+
   process.argv.forEach((key) => {
     if (key.startsWith("--")) {
       key = key.slice(11);
-      console.log(
-        "Welcome to the File Manager,",
-        key
-          .split(/[ _-]/g)
-          .map((a) => a.charAt(0).toUpperCase() + a.slice(1))
-          .join(" ")
-          .concat("!"),
-      );
+      username = key
+        .split(/[ _-]/g)
+        .map((a) => a.charAt(0).toUpperCase() + a.slice(1))
+        .join(" ");
+      console.log("Welcome to the File Manager,", username.concat("!"));
     }
   });
+
+  process.stdin.on("data", (input) => {
+    const trimmedInput = input.toString("utf8").trim();
+    if (trimmedInput === ".exit") {
+      handleExit(username);
+    } else {
+      process.stdout.write(`Received input: ${trimmedInput}\n`);
+    }
+  });
+
+  ["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) =>
+    process.on(signal, () => {
+      handleExit(username);
+    }),
+  );
 }
 
 export default hello;
